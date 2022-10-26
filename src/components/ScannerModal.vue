985ce1754080d1827c2598b2f9390c2177996f9c
rushikeshmahavirdesai <rushikeshmahavirdesai.com>
2022-10-26 13:36:19 +0530

    test

--- src/components/ScannerModal.vue
+++ src/components/ScannerModal.vue
@@ -129,8 +129,12 @@ export default {
 }
 .viewport canvas,
 .viewport video {
+  width: 100%;
+  /* height: 100%; */
   position: absolute;
-  left: 0;
-  top: 0;
+  /* height: 100%; */
+  /* position: absolute;
+	left: 0;
+	top: 0; */
 }
 </style>
